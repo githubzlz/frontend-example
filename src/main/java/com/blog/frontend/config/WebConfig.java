0b712869383be63887458174f63e71c5b0d66843
朱林忠 <sdbz_zlz@163.com>
2020-11-04 09:48:16 +0800

    本地代码上传

--- src/main/java/com/blog/frontend/config/WebConfig.java
+++ src/main/java/com/blog/frontend/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.blog.frontend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
